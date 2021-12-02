6a00f7632e1c995296862550e7f6949f9d3157d6
Yuni <amy123chou@gmail.com>
2021-12-02 23:21:30 +0800

    #fix: picture size

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -1,40 +1,41 @@
 <script>
-import LittleBtn from "@/components/LittleBtn/LittleBtn";
-import FamModal from "@/components/FamModal/FamModal";
-import AboutSwiper from "@/components/AboutSwiper/AboutSwiper";
-import FooterSection from "@/components/FooterSection/FooterSection";
+import LittleBtn from '@/components/LittleBtn/LittleBtn'
+import FamModal from '@/components/FamModal/FamModal'
+import AboutSwiper from '@/components/AboutSwiper/AboutSwiper'
+import FooterSection from '@/components/FooterSection/FooterSection'
 
 export default {
   data() {
     return {
       fam_modal: false,
-      personality_list: ["Ambition", "Creative", "Enthusiasm", "Improving"],
+      personality_list: ['Ambition', 'Creative', 'Enthusiasm', 'Improving'],
       skill_list: [
-        { name: "vue", text: "Vue" },
-        { name: "nuxt", text: "Nuxt" },
-        { name: "boot", text: "Bootstrap5" },
-        { name: "git", text: "Git" },
-        { name: "js", text: "Javascript" },
-        { name: "css", text: "CSS3 Animation" },
-        { name: "sass", text: "Scss / Sass" },
-        { name: "gsap", text: "Gsap" },
+        { name: 'vue', text: 'Vue' },
+        { name: 'nuxt', text: 'Nuxt' },
+        { name: 'react', text: 'React' },
+        { name: 'boot', text: 'Bootstrap5' },
+        { name: 'git', text: 'Git' },
+        { name: 'js', text: 'Javascript' },
+        { name: 'css', text: 'CSS3 Animation' },
+        { name: 'sass', text: 'Scss / Sass' },
+        { name: 'gsap', text: 'Gsap' },
       ],
       com_list: [
-        { name: "uiux", text: "UI/UX" },
-        { name: "visual", text: "Visual Design software" },
-        { name: "ae", text: "Motion Graphic" },
+        { name: 'uiux', text: 'UI/UX' },
+        { name: 'visual', text: 'Visual Design software' },
+        { name: 'ae', text: 'Motion Graphic' },
       ],
-    };
-  },
-  methods:{
-    toggleModal(tag){
-      this.fam_modal = tag;
     }
   },
-  computed:{
-    en_show(){
-      return this.$i18n.locale !== 'en';
-    }
+  methods: {
+    toggleModal(tag) {
+      this.fam_modal = tag
+    },
+  },
+  computed: {
+    en_show() {
+      return this.$i18n.locale !== 'en'
+    },
   },
   components: {
     AboutSwiper,
@@ -42,10 +43,10 @@ export default {
     LittleBtn,
     FamModal,
   },
-  mounted(){
-    document.title = "About Me - YUNI's PORTFOLIO";
-  }
-};
+  mounted() {
+    document.title = "About Me - YUNI's PORTFOLIO"
+  },
+}
 </script>
 
 <template src="./template.html"></template>
